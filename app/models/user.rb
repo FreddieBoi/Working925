f893d5d6a00b4814f2bc0ec79231c5a919004237
Freddie <freddieboi@gmail.com>
2011-07-02 15:23:33 +0200

    Removed the invalid default value for worked_on (date) in reports

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,21 @@
 # == Schema Information
-# Schema version: 20110611113447
+# Schema version: 20110702131552
 #
 # Table name: users
 #
-#  id                     :integer         not null, primary key
+#  id                     :integer         primary key
 #  email                  :string(255)     default(""), not null
 #  encrypted_password     :string(128)     default(""), not null
 #  reset_password_token   :string(255)
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
+#  reset_password_sent_at :timestamp
+#  remember_created_at    :timestamp
 #  sign_in_count          :integer         default(0)
-#  current_sign_in_at     :datetime
-#  last_sign_in_at        :datetime
+#  current_sign_in_at     :timestamp
+#  last_sign_in_at        :timestamp
 #  current_sign_in_ip     :string(255)
 #  last_sign_in_ip        :string(255)
-#  created_at             :datetime
-#  updated_at             :datetime
+#  created_at             :timestamp
+#  updated_at             :timestamp
 #
 
 class User < ActiveRecord::Base

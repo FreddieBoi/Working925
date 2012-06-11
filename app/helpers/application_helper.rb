9415698e95abf7ba43a59c799951e206e58e0b66
Freddie <freddieboi@gmail.com>
2012-06-11 20:57:23 +0200

    Added some comments to app helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
+  # Get the title of the app.
   def app_title
     "Working925"
   end
 
+  # Set the title of the page.
   def title(page_title)
     content_for(:title) { page_title }
   end

e8bf339ea9a563d7442d97ba9a48c80b9c4501ab
Freddie <freddieboi@gmail.com>
2012-06-11 20:44:06 +0200

    Corrected typo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
-  # Get the gravatar image for the specified User
+  # Get the Gravatar image for the specified User
   def gravatar_for(user, options = { :size => 50 })
     gravatar_image_tag(user.email.downcase, :alt => user.email,
                                             :class => 'gravatar',
                                             :gravatar => options)
   end
 
-  # Check if the specifiedd User is the current User
+  # Check if the specified User is the current User
   def current_user?(user)
     user == current_user
   end

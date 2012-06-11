b13e917765c8305471099f4ea51d2cd9f0ddfa1f
Freddie <freddieboi@gmail.com>
2012-06-11 20:57:37 +0200

    Only allow standard requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Only render standard request
+  before_filter :allow_only_standard_requests
+
+  private
+  # Only render standard request (eg. ignore reports/1.lolcat). Allow html, js, json, xml, rss. Render 404 if not allowed.
+  def allow_only_standard_requests
+    if params[:format] && (["html", "js", "json", "xml", "rss"].exclude?(params[:format]))
+      raise ActionController::RoutingError.new('No route matches "'+request.url+'"')
+    end
+  end
 end
